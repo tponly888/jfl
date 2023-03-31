f097437c09489a75db0728d44c5075fadcc83a1f
tponly888 <129528814+tponly888@users.noreply.github.com>
2023-03-31 23:09:39 +0800

    Add files via upload

---
+++ 聚丰龙工具/特性/Settings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace JuFengLongTools.Properties
+{
+	// Token: 0x0200002B RID: 43
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+	[CompilerGenerated]
+	internal sealed partial class Settings : ApplicationSettingsBase
+	{
+		// Token: 0x040000B6 RID: 182
+		[NonSerialized]
+		private string string_0 = "";
+	}
+}
